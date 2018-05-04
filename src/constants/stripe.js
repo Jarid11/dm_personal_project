bebc2183c8d6a7b39bb989e47f65fbe8ecf5b4b2
Jarid11 <Marinosj1995@gmail.com>
2018-05-04 17:17:18 -0500

    production push

--- src/constants/stripe.js
+++ src/constants/stripe.js
@@ -1,6 +1,6 @@
 const STRIPE_PUBLISHABLE =
   process.env.NODE_ENV === "production"
-    ? "pk_live_MY_PUBLISHABLE_KEY"
+    ? "pk_test_FPSUBLCiT8839ze0GX9dEQZJ"
     : "pk_test_FPSUBLCiT8839ze0GX9dEQZJ";
 
 export default STRIPE_PUBLISHABLE;
